76f113c2016f500e6273002eb9f844991434ea31
Brian Anderson <brian.anderson@catonetworks.com>
2024-10-18 20:43:13 -0700

    updated required variables with default values

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
 # Changelog
 
-## 0.0.1 (2024-10-18)
+## 0.0.2 (2024-10-18)
 
 ### Features
 - Initial commit with single socket instance with 3 NICs
+- Added default vales for required parameters
 
--- variables.tf
+++ variables.tf
@@ -8,13 +8,13 @@ variable "baseurl" {
 variable "token" {
   description = "Cato API token"
   type        = string
-  default     = null
+  default     = "xxxxxxxx"
 }
 
 variable "account_id" {
   description = "Cato account ID"
   type        = number
-  default     = null
+  default     = "12345"
 }
 
 ## vSocket Module Varibables
@@ -25,8 +25,8 @@ variable "site_description" {
 
 variable "site_name" {
   type        = string
-  description = "Your Cato Deployment Name Here"
-  default     = null
+  description = "Your Cato Site Deployment Name Here"
+  default     = "AWS vSocket site"
 }
 
 variable "native_network_range" {
@@ -35,6 +35,7 @@ variable "native_network_range" {
   	Choose the unique network range your vpc is configured with for your socket that does not conflict with the rest of your Wide Area Network.
     The accepted input format is Standard CIDR Notation, e.g. X.X.X.X/X
 	EOT
+  default     = "10.0.0.0/16"
 }
 
 variable "site_type" {
@@ -44,6 +45,7 @@ variable "site_type" {
     condition     = contains(["DATACENTER", "BRANCH", "CLOUD_DC", "HEADQUARTERS"], var.site_type)
     error_message = "The site_type variable must be one of 'DATACENTER','BRANCH','CLOUD_DC','HEADQUARTERS'."
   }
+  default = "CLOUD_DC"
 }
 
 variable "site_location" {
@@ -53,14 +55,19 @@ variable "site_location" {
     state_code   = string
     timezone     = string
   })
-  default = null
+  default = {
+    city         = "New York"
+    country_code = "US"
+    state_code   = "US-NY" ## Optinal, for coutries with states only
+    timezone     = "America/New_York"
+  }
 }
 
 ## Virtual Socket Variables
 variable "vpc_id" {
   description = ""
   type        = string
-  default     = null
+  default     = "vpc-12345"
 }
 
 variable "connection_type" {
@@ -94,35 +101,35 @@ variable "instance_type" {
 variable "key_pair" {
   description = "Name of an existing Key Pair for AWS encryption"
   type        = string
-  default     = null
+  default     = "your-key-pair-name-here"
 }
 
 variable "region" {
   description = "AWS region"
   type        = string
-  default = null
+  default     = "us-east-1"
 }
 
 variable "mgmt_eni_id" {
   description = "Managent Elastic Network Interface ID, network interface connected public to a subnet with routable access to the internet to access the internet and the Cato SASE cloud platform. Example: eni-abcde12345abcde12345"  
   type        = string
-  default     = null
+  default     = "eni-abcde12345abcde-mgmt"
 }
 
 variable "wan_eni_id" {
   description = "WAN Elastic Network Interface ID, network interface connected to a public subnet with routable access to the internet to access the internet and the Cato SASE cloud platform. Example: eni-abcde12345abcde12345"  
   type        = string
-  default     = null
+  default     = "eni-abcde12345abcde-wan"
 }
 
 variable "lan_eni_id" {
   description = "LAN Elastic Network Interface ID, network interface connected to a private subnet for local VPC resources to connect to for access to internet and WAN access through the Cato socket. Example: eni-abcde12345abcde12345"  
   type        = string
-  default     = null
+  default     = "eni-abcde12345abcde-lan"
 }
 
 variable "lan_local_ip" {
   description = "Choose an IP Address within the LAN Subnet. You CANNOT use the first four assignable IP addresses within the subnet as it's reserved for the AWS virtual router interface used as the default route for private resources to gain access to WAN and internet. The accepted input format is X.X.X.X"
   type        = string
-  default     = null
+  default     = "10.0.3.5"
 }
